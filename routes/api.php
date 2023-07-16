952cc4138f786797bc6bb83f9cd313f217d01170
sameh doush <48515888+samehdoush@users.noreply.github.com>
2023-07-16 15:00:42 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,24 +5,26 @@ use Samehdoush\LaravelTranslationsApi\Http\Controllers\TranslationController;
 
 
 Route::middleware('api')->prefix('api')->group(function () {
-    Route::controller(TranslationController::class)->prefix(config('translations.route_prefix'))
-        ->as('translations.')
-        ->group(function () {
-            Route::get('/', 'index')->name('index');
-            Route::delete('delete/{translation}', 'delete')->name('delete');
-            Route::get('{translation}/progress', 'getTranslationProgressPercentage')->name('progress');
-            // createSourceKey
-            Route::post('createSourceKey', 'createSourceKey')->name('create');
-            // createTranslation
-            Route::post('createTranslation', 'createTranslation')->name('create');
+    Route::prefix(config('translations.route_prefix'))->group(function () {
+        Route::controller(TranslationController::class)
+            ->as('translations.')
+            ->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::delete('delete/{translation}', 'delete')->name('delete');
+                Route::get('{translation}/progress', 'getTranslationProgressPercentage')->name('progress');
+                // createSourceKey
+                Route::post('createSourceKey', 'createSourceKey')->name('create');
+                // createTranslation
+                Route::post('createTranslation', 'createTranslation')->name('create');
 
-            Route::prefix('phrases')
-                ->as('phrases.')
-                ->group(function () {
-                    Route::get('{translation}', 'phrases')->name('index');
-                    Route::get('{translation}/edit/{phrase:uuid}', 'phrase')->name('show');
-                    Route::put('{translation}/edit/{phrase:uuid}', 'updatePhrase')->name('update');
-                    Route::delete('{translation}/delete/{phrase:uuid}', 'deletePhrase')->name('delete');
-                });
-        });
+                Route::prefix('phrases')
+                    ->as('phrases.')
+                    ->group(function () {
+                        Route::get('{translation}', 'phrases')->name('index');
+                        Route::get('{translation}/edit/{phrase:uuid}', 'phrase')->name('show');
+                        Route::put('{translation}/edit/{phrase:uuid}', 'updatePhrase')->name('update');
+                        Route::delete('{translation}/delete/{phrase:uuid}', 'deletePhrase')->name('delete');
+                    });
+            });
+    });
 });
